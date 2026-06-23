e15146b83675b1bc9f394752e95d03d1b28d0c59
Tran Dinh Si <trandinhsi1401@gmail.com>
2026-06-23 21:50:34 +0700

    Thêm một số valid cho tạo hoá đơn

--- src/main/java/com/iuh/pharmacy_project/dto/request/InvoiceCreationRequest.java
+++ src/main/java/com/iuh/pharmacy_project/dto/request/InvoiceCreationRequest.java
@@ -1,5 +1,7 @@
 package com.iuh.pharmacy_project.dto.request;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 import java.util.List;
@@ -12,9 +14,17 @@ import java.util.List;
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class InvoiceCreationRequest {
+    @NotBlank(message = "Employee ID is required")
     String employeeId;
+    @NotBlank(message = "Customer ID is required")
     String customerId;
+
     boolean isBankTransfer;
+
+    @NotBlank(message = "Total amount is required")
     double totalAmount;
+
+    @NotBlank(message = "Invoice details are required")
+    @Valid
     List<InvoiceDetailRequest> details;
 }
--- src/main/java/com/iuh/pharmacy_project/dto/request/InvoiceDetailRequest.java
+++ src/main/java/com/iuh/pharmacy_project/dto/request/InvoiceDetailRequest.java
@@ -1,5 +1,7 @@
 package com.iuh.pharmacy_project.dto.request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -9,8 +11,17 @@ import lombok.experimental.FieldDefaults;
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class InvoiceDetailRequest {
+    @NotBlank(message = "Product ID is required")
     String productId;
+
+    @NotBlank(message = "Unit ID is required")
     String unitId;
+
+    @NotBlank(message = "Quantity is required")
+    @Min(value = 1, message = "Quantity must be at least 1")
     int quantity;
+
+    @NotBlank(message = "Price is required")
+    @Min(value = 0, message = "Price must be a positive number")
     double price;
 }
